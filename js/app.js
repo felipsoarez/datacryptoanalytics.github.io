95740a30ea0cd5cfa547b87e312e477ab3694ade
DataCrypto Analytics <62164241+datacryptoanalytics@users.noreply.github.com>
2020-07-29 23:59:23 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -127,6 +127,31 @@ function signUp(){
     }
 }
 // xxxxxxxxxx Working For Sign In Form xxxxxxxxxx
+
+function TwitterAuthProvider(){
+var provider = new firebase.auth.TwitterAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+  // You can use these server side with your app's credentials to access the Twitter API.
+  var token = result.credential.accessToken;
+  var secret = result.credential.secret;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
+
+
 // xxxxxxxxxx Sign In Email Validation xxxxxxxxxx
 function checkUserSIEmail(){
     var userSIEmail = document.getElementById("userSIEmail");
