ccbdf4bb7cb429665f192e69c592b1cdc4eb5c52
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-11-10 16:59:39 -0400

    alt load

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,4 +1,4 @@
 jQuery(window).load(function () {
-  $(".loader").delay(1500).fadeOut("slow"); //retire o delay quando for copiar!
+  $(".loader").delay(15).fadeOut("slow"); //retire o delay quando for copiar!
 $("#tudo_page").toggle("fast");
 });
