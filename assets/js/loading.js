628aa33ba6762fb9aaa7443a4b826f5a0463c2b7
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-11-10 17:25:10 -0400

    remove delay

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -2,6 +2,6 @@
         // Este evendo é acionado após o carregamento da página
         jQuery(window).load(function() {
             //Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
-            jQuery("#loader").delay(2000).fadeOut("slow");
+            jQuery("#loader").fadeOut("slow");
         });
  
