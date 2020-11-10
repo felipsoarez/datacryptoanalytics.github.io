65c7388aa2bb958b641bb19103edbf7f59bc8e56
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-11-10 16:53:29 -0400

    alt load

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,7 +1,1 @@
-$(document).ready(function () {
-  $("#divCarregando").show();
-  $(window).load(function () {
-      // Quando a página estiver totalmente carregada, remove o id
-      $('#divCarregando').fadeOut('slow');
-  });
-});
+onclick="$('#loading').show();"
