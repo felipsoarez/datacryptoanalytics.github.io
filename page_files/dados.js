8f528fc76a545c3f71dbc0e64217606f4664a260
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-11-24 09:10:30 -0400

    a

--- page_files/dados.js
+++ page_files/dados.js
@@ -1,7 +1,7 @@
 const itez = 'https://pay.itez.com/static/main/share/merchant.js?';
 
 
-const requisica = fetch(itez, {mode: 'cors'})
+const requisica = fetch(itez, {mode: 'no-cors'})
     .then(Response => {
         return Response.json();
         console.log(requisica)
