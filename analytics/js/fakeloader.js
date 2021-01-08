a9c70e1a2c8c525990da1d6924dacf5ff07a0518
datacryptoanalytics <felipe_soarez@live.com>
2021-01-08 18:35:34 -0400

    alt time

--- analytics/js/fakeloader.js
+++ analytics/js/fakeloader.js
@@ -1,5 +1,5 @@
 $(".fakeloader").fakeLoader({
-    timeToHide:1200, //Time in milliseconds for fakeLoader disappear
+    timeToHide:5200, //Time in milliseconds for fakeLoader disappear
     zIndex:"999",//Default zIndex
     spinner:"spinner3",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
     bgColor:"#2ecc71", //Hex, RGB or RGBA colors
