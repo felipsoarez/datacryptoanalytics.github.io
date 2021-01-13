6af9cc80797cfeae7da7b55256b5a009dfa8e862
datacryptoanalytics <felipe_soarez@live.com>
2021-01-13 09:37:05 -0400

    alt simples

--- analytics/js/main.js
+++ analytics/js/main.js
@@ -4,9 +4,9 @@ $(function() {
 
     $('.fakeLoader').fakeLoader({
 
-        timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
+        timeToHide: 3200, //Time in milliseconds for fakeLoader disappear
 
-        zIndex: "999",//Default zIndex
+        zIndex: "99",//Default zIndex
 
         spinner: "spinner3",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
 
