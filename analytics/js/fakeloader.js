7557204642fbcf6cdc592d071909b1a75a87a6ce
datacryptoanalytics <felipe_soarez@live.com>
2021-01-13 10:14:36 -0400

    al z-index

--- analytics/js/fakeloader.js
+++ analytics/js/fakeloader.js
@@ -1,6 +1,6 @@
 $(".fakeloader").fakeLoader({
     timeToHide:7200, //Time in milliseconds for fakeLoader disappear
-    zIndex:"9999",//Default zIndex
+    zIndex:"9",//Default zIndex
     spinner:"spinner3",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
     bgColor:"#2ecc71", //Hex, RGB or RGBA colors
     //imagePath:"yourPath/customizedImage.gif" //If you want can you insert your custom image
--- analytics/js/main.js
+++ analytics/js/main.js
@@ -6,7 +6,7 @@ $(function() {
 
         timeToHide: 7200, //Time in milliseconds for fakeLoader disappear
 
-        zIndex: "9999",//Default zIndex
+        zIndex: "9",//Default zIndex
 
         spinner: "spinner3",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
 
