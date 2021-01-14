3fc81e081edb7ed7525c2fc7333f206bf7c14e02
datacryptoanalytics <felipe_soarez@live.com>
2021-01-14 13:33:21 -0400

    alt time

--- analytics/js/main.js
+++ analytics/js/main.js
@@ -4,9 +4,9 @@ $(function() {
 
     $('.fakeLoader').fakeLoader({
 
-        timeToHide: 7200, //Time in milliseconds for fakeLoader disappear
+        timeToHide: 3200, //Time in milliseconds for fakeLoader disappear
 
-        zIndex: "9",//Default zIndex
+        zIndex: "9999",//Default zIndex
 
         spinner: "spinner3",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
 
