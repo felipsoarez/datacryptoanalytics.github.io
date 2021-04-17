d8fa78c24fcac7a3b77728d830c2bef455bc5368
datacryptoanalytics <felipe_soarez@live.com>
2021-04-17 19:34:21 -0400

    alt menu

---
+++ analytics/js/api.js
@@ -0,0 +1,25 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.flipsidecrypto.com/api/v2/metrics/projects?api_key=d299fd8d-21c1-48ca-9325-861279e68a33")
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (data) {
+      appendData(data);
+      console.log(data);
+  })
+  .catch(function (err) {
+      console.log('error: ' + err);
+  });
+function appendData(data) {
+  var mainContainer = document.getElementById("myData");
+  for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.innerHTML = 'Name: ' + data[i].name + ' ' + data[i].symbol;
+      mainContainer.appendChild(div);
+  }
+}
+  
