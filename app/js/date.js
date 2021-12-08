82dbcb0964298956d84fe2c399429b09b924d106
Felipe Soares <felipe_soarez@live.com>
2021-12-08 11:33:27 -0300

    alt date

--- app/js/date.js
+++ app/js/date.js
@@ -1,11 +1,7 @@
 dayName = new Array ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
 monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
 now = new Date
-
-
 var clock = document.getElementById('real-clock');
-    
-
 setInterval(function () {
     clock.innerHTML = ((new Date).toLocaleString().substr(11, 8));  
 }, 1000);
