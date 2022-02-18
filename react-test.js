d649a3bf9ecce49ac5757c1e9aa14f4e40f6212b
Felipe Soares <felipe_soarez@live.com>
2022-02-18 13:55:47 -0400

    aotds

--- react-test.js
+++ react-test.js
@@ -270,3 +270,28 @@ var intro = React.createElement({
       );
     }
   });
+
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
