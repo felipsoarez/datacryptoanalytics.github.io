dbb90d7ff601c580d7786950656257b01cf9a3c1
Felipe <felipe_soarez@live.com>
2023-03-25 15:31:18 -0400

    add bg

--- js/main.js
+++ js/main.js
@@ -91,3 +91,20 @@ $(function() {
 
 
 
+// Remove the transition class
+const square = document.querySelector('.square');
+square.classList.remove('square-transition');
+
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('square-transition');
+      return;
+    }
+
+    square.classList.remove('square-transition');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper'));
