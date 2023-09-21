875938854ae5a0ea8f21ec03be6ae3177e5e6ac9
Felipe <felipe_soarez@live.com>
2023-09-21 14:37:04 -0400

    rmv

--- app/js/fakeloader.js
+++
@@ -1,5 +0,0 @@
-$(".fakeloader").fakeLoader({
-    timeToHide:7200, //Time in milliseconds for fakeLoader disappear
-    bgColor:"#121212", //Hex, RGB or RGBA colors
-    //imagePath:"yourPath/customizedImage.gif" //If you want can you insert your custom image
-  });
